ace0d0dd7c2c5cb9799ac7a96d57769452071603
Frode Aannevik <frode@dintero.com>
2022-10-24 22:49:54 +0200

    Bump json-schema-faker from  0.5.0-rc16 to 0.5.0-rcv.46 (#13)
    Co-authored-by: Frode Aannevik <frodea@dintero.com>

--- test/test-api.js
+++ test/test-api.js
@@ -456,7 +456,7 @@ function runTests (mode) {
 
               tHelpers.shouldHadFailed();
             } catch (err) {
-              assert.equal(err.message, 'unknown registry key "sway" in /');
+              assert.equal(err.message.substring(0, 34), 'Error: unknown registry key "sway"');
             }
 
             // Register the custom format
